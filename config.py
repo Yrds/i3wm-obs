07b3bc6deaf26be3a1dd3303ec80328254613e4b
Yuri Santos <yrds96@protonmail.com>
2020-07-23 15:22:03 -0300

    config.py parameters order

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-source = 'i3wm' #Source on obs that you want to control
 host = '127.0.0.1'
 port = '4444'
 password = 'obsremote'
+
+source = 'i3wm' #Source on obs that you want to control
 allowed_workspaces = ['3', '4'] #allowed_workspaces that you want to show when using obs, the others will be blocked
